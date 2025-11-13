df00ac8f3155084ff0397c88e0a2c0ed7147ef43
Tawhidul Islam <islam100tawhidul@gmail.com>
2025-11-13 00:05:16 -0500

    Initial commit of final project server

--- database/models/Student.js
+++ database/models/Student.js
@@ -6,15 +6,58 @@ It defines the student model for the database.
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
 
+const DEFAULT_IMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmRLRMXynnc7D6-xfdpeaoEUeon2FaU0XtPg&s";
+
 const Student = db.define("student", {
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false, 
+    validate: {
+      notEmpty: true
+    }
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false, 
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  email: {
+    type: Sequelize.STRING, 
+    allowNull: false, 
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  }, 
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,  // <-- always required
+    defaultValue: DEFAULT_IMAGE,
+    validate: {
+      isURL: true
+    }
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL(2,1), 
+    allowNull: true, 
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
+  }
+}, {
+  hooks: {
+    beforeValidate: (student) => {
+      if (!student.imageURL || student.imageURL.trim() === "") {
+        student.imageURL = DEFAULT_IMAGE;
+      }
+    }
   }
 });
 
--- database/utils/configDB.js
+++ database/utils/configDB.js
@@ -6,8 +6,8 @@ It declares and exports the variables for database name, username, and password.
 // Declare the variables for database name, username, and password.
 const dbName = 'starter-server';
 const dbUser = 'postgres';
-const dbPwd = 'postgres';  // Note: If needed, change this password to match the password created for PostgreSQL database on the local machine.
-
+const dbPwd = '24196826';  // This is password for testing locally  for postpresql to work
+//const dbPwd = 'postgres';  // This was the orginal
 // Export the variables 
 module.exports = {
   dbName,
